52d44f6707f3c81cd0383b871e9aad715962f161
Martina Slabejova <485593@fi.muni.cz>
2020-11-20 13:06:06 +0100

    TripQueryObject

--- PV179-Project/BusinessLayer/QueryObjects/TripQueryObject.cs
+++ PV179-Project/BusinessLayer/QueryObjects/TripQueryObject.cs
@@ -1,7 +1,35 @@
+using AutoMapper;
+using BusinessLayer.DataTransferObjects;
+using BusinessLayer.DataTransferObjects.Filters;
+using DataAccessLayer.DataClasses;
+using Infrastructure.Query;
+
 namespace BusinessLayer.QueryObjects
 {
-    public class TripQueryObject
+    public class TripQueryObject : QueryObjectBase<Trip, TripDto, TripFilterDto, IQuery<Trip>>
     {
-        
+        public TripQueryObject(IMapper mapper, IQuery<Trip> query) : base(mapper, query)
+        {
+            
+        }
+
+        public override IQuery<Trip> ApplyFilter(IQuery<Trip> query, TripFilterDto filter)
+        {
+            /*query = string.IsNullOrWhiteSpace(filter.StartDate)
+                ? query
+                : ((TripQuery) query).FilterByStartDate();
+            */
+            query = string.IsNullOrWhiteSpace(filter.Done)
+                ? query
+                : ((TripQuery) query).FilterByDone(bool.Parse(filter.Done));
+            query = string.IsNullOrWhiteSpace(filter.AuthorId)
+                ? query
+                : ((TripQuery) query).FilterByAuthorId(int.Parse(filter.AuthorId));
+            query = string.IsNullOrWhiteSpace(filter.Title)
+                ? query
+                : ((TripQuery) query).FilterByTitle(filter.Title);
+            return query;
+            
+        }
     }
 }
