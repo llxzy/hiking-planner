367470bbef419d975a0d5e8fead2c99d93d6d329
llxzy <snowcrash@tuta.io>
2020-11-21 19:57:36 +0100

    added a mapper configuration

---
+++ PV179-Project/BusinessLayer/MappingConfig.cs
@@ -0,0 +1,24 @@
+using AutoMapper;
+using BusinessLayer.DataTransferObjects;
+using BusinessLayer.Services.Implementations;
+using DataAccessLayer.DataClasses;
+
+namespace BusinessLayer
+{
+    public class MappingConfig
+    {
+        public static void ConfigureMap(IMapperConfigurationExpression config)
+        {
+            config.CreateMap<Trip, TripDto>().ReverseMap();
+            config.CreateMap<User, UserDto>().ReverseMap();
+            config.CreateMap<Review, ReviewDto>().ReverseMap();
+            config.CreateMap<UserTrip, UserTripDto>().ReverseMap();
+            config.CreateMap<Location, LocationDto>().ReverseMap();
+            config.CreateMap<Challenge, ChallengeDto>().ReverseMap();
+            config.CreateMap<TripLocation, TripLocationDto>().ReverseMap();
+            config.CreateMap<UserReviewVote, UserReviewVoteDto>().ReverseMap();
+            
+        }
+        
+    }
+}
