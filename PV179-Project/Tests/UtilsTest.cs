dd39bf0b3c67294583d4715f3662ed439b2daed2
llxzy <snowcrash@tuta.io>
2020-11-26 23:28:48 +0100

    added a password hashing test

--- PV179-Project/Tests/UtilsTest.cs
+++ PV179-Project/Tests/UtilsTest.cs
@@ -13,5 +13,14 @@ namespace Tests
             var actual = new DateTime(2020, 11, 17);
             Assert.Equal(actual, date);
         }
+
+        [Fact]
+        public void PasswordEncodeTest()
+        {
+            const string password = "TotallySecureUnbreakablePassword1337";
+            const string expected = 
+                "A6F21D7638BD9A36A67DFEB9CCBCB719CD588EA43A76B93ABD6E687114B47C6DE8F4803E70B5EAC827BCA3C39928AC9DB148F7D354F8772AA6C5B508CAD783EF";
+            Assert.True(HashingUtils.Validate(password, expected));
+        }
     }
 }
