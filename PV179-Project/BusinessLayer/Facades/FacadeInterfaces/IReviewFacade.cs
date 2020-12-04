609e5169931c6990b1d1f50eb98ec43c42923229
llxzy <snowcrash@tuta.io>
2020-12-04 15:16:36 +0100

    added methods to review facade interface

--- PV179-Project/BusinessLayer/Facades/FacadeInterfaces/IReviewFacade.cs
+++ PV179-Project/BusinessLayer/Facades/FacadeInterfaces/IReviewFacade.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BusinessLayer.DataTransferObjects;
 
 namespace BusinessLayer.Facades.FacadeInterfaces
 {
     public interface IReviewFacade : IDisposable
     {
+        Task Create(string text, int tripId, int userId);
+        Task Delete(int reviewId);
+        Task VoteReview(bool up, int reviewId, int userId);
+        List<ReviewDto> ListAuthorReviews(int authorId);
+        List<ReviewDto> ListUpvotedReviews(int? authorId, int? tripId);
+        List<ReviewDto> ListDownvoted(int? authorId, int? tripId);
+        List<ReviewDto> ListFlagged(int userId, int? authorId, int? tripId);
+        List<ReviewDto> ListReviewsByTrip(int tripId, int? authorId);
         
     }
 }
