1e08b026c2d518a07b4f049aea06864771eafb07
llxzy <snowcrash@tuta.io>
2020-12-09 23:09:42 +0100

    working dependency injection with autofac

--- PV179-Project/API/Startup.cs
+++ PV179-Project/API/Startup.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using BusinessLayer;
 using BusinessLayer.DataTransferObjects;
 using BusinessLayer.DataTransferObjects.Filters;
 using BusinessLayer.DataTransferObjects.QueryDTOs;
@@ -38,40 +42,11 @@ namespace API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // original public void ConfigureServices(IServiceCollection services)
         public void ConfigureServices(IServiceCollection services)
         { 
-            // autofac?
-            
-            services.AddScoped<IUserFacade, UserFacade>();
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IUnitOfWorkProvider, UnitOfWorkProvider>();
-            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
-            
-            // no idea if correct but works
-            services.AddScoped<FilterDtoBase, UserFilterDto>();
-            services.AddScoped<FilterDtoBase, TripFilterDto>();
-            services.AddScoped<FilterDtoBase, ChallengeFilterDto>();
-            services.AddScoped<FilterDtoBase, ReviewFilterDto>();
-            services.AddScoped<FilterDtoBase, LocationFilterDto>();
-
-
-            services.AddScoped(typeof(IQuery<User>), typeof(UserQuery));
-            services.AddScoped(typeof(IQuery<Review>), typeof(ReviewQuery));
-            services.AddScoped(typeof(IQuery<Challenge>), typeof(ChallengeQuery));
-            services.AddScoped(typeof(IQuery<Trip>), typeof(TripQuery));
-            services.AddScoped(typeof(IQuery<Location>), typeof(LocationQuery));
-           // query obj base
-           // problem with abstract?
-           // below dont work
-           //services.AddScoped<QueryObjectBase<User, UserDto, UserFilterDto, UserQuery>,typeof(UserQueryObject)>();
-           //services.AddScoped(typeof(QueryObjectBase<User, UserDto, UserFilterDto, UserQuery>), typeof(UserQueryObject));
-            
-            services.AddScoped<DbContext, DatabaseContext>();
-            services.AddSingleton(provider => 
-                new Func<DbContext>(() => provider.GetService<DbContext>()));
-            
-            //services.AddDbContext<DatabaseContext>(options => options
-            //    .UseNpgsql(@"Host=localhost;Database=tripdb;Username=postgres;Password=postgres;Port=5432"));
+            //services.AddDbContext<DatabaseContext>(options => options"))
+            //    .UseNpgsql(@"Host=localhost;Database=tripdb;Username=postgres;Password=postgres;Port=5432;
             services.AddApiVersioning(x =>
             {
                 x.DefaultApiVersion = new ApiVersion(1, 0);
@@ -83,7 +58,12 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
-            
+            services.AddAutofac();
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new AutofacBusinessLayerConfig());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
