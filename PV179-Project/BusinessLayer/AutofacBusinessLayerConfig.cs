9a126292298f65d3e7d5600d245ef1cba055451c
llxzy <snowcrash@tuta.io>
2020-12-10 15:43:45 +0100

    autofac configurations

---
+++ PV179-Project/BusinessLayer/AutofacBusinessLayerConfig.cs
@@ -0,0 +1,59 @@
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using BusinessLayer.DataTransferObjects;
+using BusinessLayer.DataTransferObjects.Filters;
+using BusinessLayer.QueryObjects;
+using BusinessLayer.Services;
+using DataAccessLayer.DataClasses;
+using Infrastructure;
+using Infrastructure.Query;
+using Infrastructure.UnitOfWork;
+
+namespace BusinessLayer
+{
+    public class AutofacBusinessLayerConfig : Autofac.Module
+    {
+        //public static IContainer Configure()
+        protected override void Load(ContainerBuilder builder)
+        {
+            //var builder = new ContainerBuilder();
+            builder.RegisterModule(new AutofacInfrastructureConfig());
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => t.Namespace == "BusinessLayer.QueryObjects").AsSelf()
+                .InstancePerDependency();
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => t.Namespace == "BusinessLayer.Services.Implementations")
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name))
+                .InstancePerDependency();
+            
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => t.Namespace == "BusinessLayer.Facades.FacadeImplementations")
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name))
+                .InstancePerDependency();
+
+            builder.RegisterGeneric(typeof(QueryObjectBase<,,,>))
+                .As(typeof(IQueryObjectBase<,,>)).InstancePerDependency();
+
+            builder.RegisterType<UserQueryObject>()
+                //.AsSelf().InstancePerDependency();
+                .As<QueryObjectBase<User, UserDto, UserFilterDto, IQuery<User>>>().InstancePerDependency();
+            builder.RegisterType<ReviewQueryObject>()
+                .AsSelf().InstancePerDependency();
+            builder.RegisterType<TripQueryObject>()
+                .AsSelf().InstancePerDependency();
+            builder.RegisterType<LocationQueryObject>()
+                .AsSelf().InstancePerDependency();
+            builder.RegisterType<ChallengeQueryObject>()
+                .AsSelf().InstancePerDependency();
+            
+            // register generic doesn't work with crud query :( 
+            // but ^^ should work?
+
+            // filters? dtos? probably don't need to, just a reminder to add it here if stuck 
+            //return builder.Build();
+        }
+    }
+}
---
+++ PV179-Project/Infrastructure/AutofacInfrastructureConfig.cs
@@ -0,0 +1,38 @@
+using System;
+using Autofac;
+using DataAccessLayer;
+using DataAccessLayer.DataClasses;
+using Infrastructure.Query;
+using Infrastructure.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class AutofacInfrastructureConfig : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UnitOfWork.UnitOfWork>()
+                .As<IUnitOfWork>().InstancePerDependency();
+            builder.RegisterType<UnitOfWorkProvider>()
+                .As<IUnitOfWorkProvider>().InstancePerDependency();
+            // should be fine for generics
+            builder.RegisterGeneric(typeof(GenericRepository<>))
+                .As(typeof(IRepository<>)).InstancePerDependency();
+            
+            // trying generic for query, alternative is
+            builder.RegisterGeneric(typeof(QueryBase<>))
+                .As(typeof(IQuery<>)).InstancePerDependency();
+            
+            // todo delete comments before pushing final
+            // queries
+            // for now not added, possible to add registerype<specific query> as self()?
+            // they are directly converted in queryobj base so who knows
+            
+            // from cviko ukazka
+            builder.RegisterType<DatabaseContext>()
+                .AsSelf().InstancePerDependency();
+
+        }
+    }
+}
