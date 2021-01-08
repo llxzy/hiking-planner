e4d312a8188e2568cec4e6f2026ea03135696bc9
llxzy <snowcrash@tuta.io>
2021-01-08 16:42:04 +0100

    uncommented collections in user model, wip on profile.cshtml

--- PV179-Project/Application/Models/UserModels/UserModel.cs
+++ PV179-Project/Application/Models/UserModels/UserModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.DataTransferObjects;
 using DataAccessLayer.Enums;
 
 namespace Application.Models.UserModels
@@ -11,9 +12,9 @@ namespace Application.Models.UserModels
         public string Name { get; set; }
         public string MailAddress { get; set; }
         public string PasswordHash { get; set; }
-        //public IEnumerable<UserTripDto> Trips { get; set; }
-        //public IEnumerable<ChallengeDto> Challenges { get; set; }
-        //public IList<UserReviewVoteDto> UserReviewVotes { get; set; }
+        public IEnumerable<UserTripDto> Trips { get; set; }
+        public IEnumerable<ChallengeDto> Challenges { get; set; }
+        public IList<UserReviewVoteDto> UserReviewVotes { get; set; }
         public UserRole Role { get; set; }
     }
 }
