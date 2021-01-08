c5c14b1f037cd27f474094a99dacba067e55c7cf
llxzy <snowcrash@tuta.io>
2021-01-08 22:59:05 +0100

    changed Ienumerable to IList

--- PV179-Project/BusinessLayer/DataTransferObjects/UserDto.cs
+++ PV179-Project/BusinessLayer/DataTransferObjects/UserDto.cs
@@ -10,8 +10,8 @@ namespace BusinessLayer.DataTransferObjects
         public string Name { get; set; }
         public string MailAddress { get; set; }
         public string PasswordHash { get; set; }
-        public IEnumerable<UserTripDto> Trips { get; set; }
-        public IEnumerable<ChallengeDto> Challenges { get; set; }
+        public IList<UserTripDto> Trips { get; set; }
+        public IList<ChallengeDto> Challenges { get; set; }
         public IList<UserReviewVoteDto> UserReviewVotes { get; set; } 
         public UserRole Role { get; set; }
     }
